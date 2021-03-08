4c19bc4f857559f56f1e8bf6d07f6107f800dc94
Alexander Leal <alex.j.leal@live.com>
2021-03-07 17:26:04 -0700

    components, footer, header, project

---
+++ src/components/Project.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Footer extends Component {
+    render(){
+        <div className='Footer'>
+        <Footer size="mini">
+            <FooterSection type="left" logo="Title">
+                <FooterLinkList>
+                    <a href="/">Help</a>
+                    <a href="/">Privacy & Terms</a>
+                </FooterLinkList>
+            </FooterSection>
+        </Footer>
+        </div>
+    }
+}
+export default Footer;
